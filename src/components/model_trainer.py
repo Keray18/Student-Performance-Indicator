dec09de06e0189cefbe2d1dc7a8e9b98ff1b65e7
Keray18 <keshavbahoray18@gmail.com>
2023-10-06 12:46:16 +0530

    updated the correct dataframe format and created web app

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,35 +1,42 @@
-import os  
-import sys  
+import os
+import sys
 from dataclasses import dataclass
 
 from catboost import CatBoostRegressor
-from sklearn.ensemble import ( AdaBoostRegressor, GradientBoostingRegressor, RandomForestRegressor )
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.metrics import r2_score
 from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
 
-from src.utils import save_object, evaluate_models
-
+from src.utils import save_object,evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_path=os.path.join("artifacts", "model.pkl")
-
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
 
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config = ModelTrainerConfig()
+        self.model_trainer_config=ModelTrainerConfig()
 
-    def initiate_model_trainer(self,train_array, test_array):
+
+    def initiate_model_trainer(self,train_array,test_array):
         try:
-            logging.info("Splitting training data and test data")
-            X_train, y_train, X_test, y_test=( train_array[:,:-1], train_array[:,-1], test_array[:,:-1], test_array[:,-1])
-            
+            logging.info("Split training and test input data")
+            X_train,y_train,X_test,y_test=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
             models = {
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
@@ -39,7 +46,6 @@ class ModelTrainer:
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
-
             params={
                 "Decision Tree": {
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
@@ -77,23 +83,26 @@ class ModelTrainer:
                 }
                 
             }
-            
-            model_report:dict=evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, params=params)
 
-            ## Getting the best model score and name
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models,param=params)
+            
+            ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
+
+            ## To get best model name from dict
+
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-
             best_model = models[best_model_name]
 
-            if best_model_score < 0.6:
-                raise CustomException("No Best Model Found!")
-            logging.info("Best Model Found on both training and test dataset")
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best found model on both training and testing dataset")
 
             save_object(
-                file_path=self.model_trainer_config.trained_model_path,
+                file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model
             )
 
@@ -103,5 +112,8 @@ class ModelTrainer:
             return r2_square
             
 
+
+
+            
         except Exception as e:
             raise CustomException(e,sys)
