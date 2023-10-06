bfb83d96a2a2c7871db304ed1f8f6e8d98bc847a
Keray18 <keshavbahoray18@gmail.com>
2023-10-06 14:11:59 +0530

    finalized

--- application.py
+++ application.py
@@ -9,8 +9,6 @@ application=Flask(__name__)
 
 app=application
 
-## Route for a home page
-
 @app.route('/')
 def index():
     return render_template('index.html') 
@@ -42,5 +40,5 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", debug=True)        
+    app.run(host="0.0.0.0")        
 
